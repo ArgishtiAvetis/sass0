19b346452ff9f782025ec26ba887e91ee482f4f6
Argishti <argishtiavetis@gamil.com>
2017-06-18 18:34:09 -0700

    firs

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.listen(8080, () => console.log("Ok"));
